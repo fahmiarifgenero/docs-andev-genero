6f8897410a6ffce742c6abbb4ac4c045d10d42bb
Fahmi Arif Genero <fahmi.arif.genero@gmail.com>
2023-08-10 07:23:44 +0700

    settings theme, add pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,19 +1,48 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s - Docs Andev Dashboard",
+      };
+    }
+  },
   logo: <span>Andev Dashboard</span>,
   project: {
     link: "https://github.com/shuding/nextra-docs-template",
   },
-  chat: {
-    link: "https://discord.com",
-  },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: "Nextra Docs Template",
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{" "}
+        <a href="https://nextra.site" target="_blank">
+          Nextra
+        </a>
+        .
+      </span>
+    ),
+  },
+  search: {
+    placeholder: "Search...",
+  },
+  sidebar: {
+    autoCollapse: false,
+    defaultMenuCollapseLevel: 1,
   },
   faviconGlyph: "📝",
+  // feedback: {
+  //   content: null,
+  // },
+  editLink: {
+    component: null,
+  },
+  navbar: {
+    extraContent: <a href="https://google.com">Hello</a>,
+  },
 };
 
 export default config;
