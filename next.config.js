be04febca6ea8bfc964e536ad971b11bbe96b581
Fahmi Arif Genero <fahmi.arif.genero@gmail.com>
2023-08-14 06:57:52 +0700

    change name to rear-view and front-view, add some text on development journey

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
+  flexSearch: {
+    codeblocks: false,
+  },
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
